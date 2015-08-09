95563c642ce130ecfc9ae81ff8342363f477ec20
Mokey <McClainG9616@Gmail.com>
2015-08-09 03:28:46 -0400

    Fix on Duplicate entry

--- client/systems/playerMenu/dialog/player_settings.hpp
+++ client/systems/playerMenu/dialog/player_settings.hpp
@@ -160,14 +160,6 @@ class playerSettings {
 			w = 0.225; h = 0.033 * safezoneH;
 		};
 
-		class btnDistanceEffects : w_RscButton {
-			idc = -1;
-			text = "Effects";
-			onButtonClick = "[] execVM 'addons\disableEnvironment\disableEnvironment.sqf'";
-			x = 0.158; y = 0.5;
-			w = 0.125; h = 0.033 * safezoneH;
-		};
-		
 		class btnDistanceNear : w_RscButton {
 			idc = -1;
 			text = "Near";
